133a2da0d657943836172a501f3c6f378953b39d
A.Lejeune <acelyalejeune@gmail.com>
2024-05-15 16:16:31 +0200

    add notes

--- add-note.php
+++ add-note.php
@@ -49,31 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['action']) && $_POST['a
         </div>
         <div class="row">
             <div class="col-12">
-                <form id="form_add_note" action="add-note.php" method="post">
-                    <div class="mb-3 form-group">
-                        <label for="title_note" class="form-label appLabel">Titre</label>
-                        <input type="text" class="form-control" name="title_note" id="title_note" placeholder="Title" required>
-                    </div>
-                    <div class="mb-3 form-group">
-                        <label for="type_note" class="form-label appLabel">Type</label>
-                        <select name="type_note" id="type_note" class="form-control" required>
-                            <option value="note">Note textuelle</option>
-                            <option value="code">Code Source</option>
-                            <option value="lien">Lien / Url</option>
-                        </select>
-                    </div>
-                    <div class="mb-3 form-group">
-                        <input class="form-check-input" type="checkbox" value="1" id="favori_note" name="favori_note">
-                        <label class="form-check-label" for="favori_note">Ajouter aux favoris</label>
-                    </div>
-                    <div class="mb-3 form-group">
-                        <label for="content_note" class="form-label appLabel" id="label_content_note">Contenu</label>
-                        <textarea name="content_note" id="content_note" class="form-control" placeholder="Content" rows="10" required></textarea>
-                    </div>
-                    <input type="hidden" name="action" value="addnote">
-                    <button type="submit" class="btn btn-outline-success">Ajouter</button>
-                    <a href="index.php" class="btn btn-outline-danger">Annuler</a>
-                </form>
+                <?php echo HTMLAddNewNoteForm(); ?>
             </div>
         </div>
 
